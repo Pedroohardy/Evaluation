48469fa28e199b2a3ee158744da46c131d5fe29e
Pierre HARDY <pierre.hardy-2@etu.univ-tours.fr>
2026-02-13 14:20:26 +0100

    05-tarifs-manager-structure

---
+++ models/_init_.py
@@ -0,0 +1,5 @@
+from .client import Client
+from .vehicule import Vehicule
+from .tarifs import TarifsManager
+
+__all__ = ["Client", "Vehicule", "TarifsManager"]
---
+++ models/tarifs.py
@@ -0,0 +1,14 @@
+class TarifsManager:
+    TARIFS = {
+        4: {},
+        5: {},
+        6: {}
+    }
+
+    @classmethod
+    def obtenir_tarif(cls, cylindree, forfait_km):
+        pass
+
+    @classmethod
+    def afficher_grille(cls):
+        pass
