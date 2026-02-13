920568ad6a78cea4ae03f594b99b35ae8d533e47
Pierre HARDY <pierre.hardy-2@etu.univ-tours.fr>
2026-02-13 15:29:12 +0100

    15-nettoyage-terminal

--- main.py
+++ main.py
@@ -6,6 +6,7 @@ from ui import (
     demander_reservation,
     afficher_reservations,
     afficher_reservations_client,
+    nettoyer_terminal,
 )
 from data_manager import charger_reservations
 from models import Reservation
@@ -13,23 +14,29 @@ from models import Reservation
 
 def main() -> None:
     while True:
+        nettoyer_terminal()
         afficher_menu()
         choix = demander_choix_menu()
 
         if choix == "1":
+            nettoyer_terminal()
             afficher_clients()
 
         elif choix == "2":
+            nettoyer_terminal()
             afficher_vehicules()
 
         elif choix == "3":
+            nettoyer_terminal()
             demander_reservation()
 
         elif choix == "5":
+            nettoyer_terminal()
             reservations = charger_reservations()
             afficher_reservations(reservations)
 
         elif choix == "6":
+            nettoyer_terminal()
             afficher_reservations_client()
 
         elif choix == "7":
