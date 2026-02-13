3e957fa757e4555e772da122e028d9207756a6a5
Pierre HARDY <pierre.hardy-2@etu.univ-tours.fr>
2026-02-13 14:08:28 +0100

    03-impl-client-vehicule-v1

---
+++ models/vehicule.py
@@ -0,0 +1,23 @@
+class Vehicule:
+    def __init__(
+        self,
+        id_vehicule: str,
+        marque: str,
+        modele: str,
+        cylindree: int,
+        kilometrage_actuel: int,
+        date_mise_en_circulation: str
+    ) -> None:
+        self.id_vehicule = id_vehicule
+        self.marque = marque
+        self.modele = modele
+        self.cylindree = cylindree
+        self.kilometrage_actuel = kilometrage_actuel
+        self.date_mise_en_circulation = date_mise_en_circulation
+
+    def __str__(self) -> str:
+        return (
+            f"{self.id_vehicule} - {self.marque} {self.modele} "
+            f"({self.cylindree} cyl., {self.kilometrage_actuel} km)"
+        )
+
